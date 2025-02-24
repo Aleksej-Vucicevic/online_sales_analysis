8b887c1ccbb4ac31f11212795427e6213832cd8a
Aleksej Vucicevic <zexilmirror@gmail.com>
2025-02-24 13:34:32 +0100

    Dodata klasa Cart i funkcionalnosti

--- main.py
+++ main.py
@@ -10,3 +10,11 @@ manager.add_product(Product("Telefon", 500, 10))
 # Prikaz proizvoda i ukupne vrednosti
 manager.display_products()
 print(f"Total Inventory Value: {manager.total_inventory_value()}")
+from cart import Cart
+
+cart = Cart()
+cart.add_to_cart(Product("Laptop", 1000, 2))
+cart.add_to_cart(Product("Telefon", 500, 1))
+
+cart.display_cart()
+print(f"Total Cart Value: {cart.calculate_total()}")
